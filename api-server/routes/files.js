4c1c440650658fe2c411736cc667e61d083cead2
Jamie Syme <jamie.d.syme@gmail.com>
2018-04-27 07:13:53 -0700

    Implemented the ability to delete a file.

--- api-server/routes/files.js
+++ api-server/routes/files.js
@@ -5,34 +5,6 @@ const bucketId = process.env.B2_BUCKET_ID;
 const upload = multer();
 
 module.exports = function (app) {
-	app.get('/files/:id/contents', async (req, res) => {
-		try {
-			// Get the file id
-			const fileId = req.params.id;
-
-			// Look up B2 fileId in Arango
-			const db = app.get('arango');
-			let b2FileId;
-			try {
-				const doc = db.collection('files').document(fileId);
-				b2FileId = doc.b2FileId;
-			} catch (e) {
-				res.sendStatus(404);
-				return;
-			}
-
-			// Download file from B2
-			const b2 = app.get('b2');
-			await b2.authorize();
-			const b2Res = await b2.downloadFileById({ fileId: b2FileId });
-
-			res.send(b2Res.data);
-
-		} catch (err) {
-			console.log(err);
-			res.sendStatus(500);
-		}
-	});
 	app.post('/files', upload.single('file'), async (req, res) => {
 		try {
 			// Upload the file to B2
@@ -62,6 +34,69 @@ module.exports = function (app) {
 			res.status(201).json({
 				id: fileId,
 			});
+
+		} catch (err) {
+			console.log(err);
+			res.sendStatus(500);
+		}
+	});
+	app.delete('/files/:id', async (req, res) => {
+		try {
+			// Get the file id
+			const fileId = req.params.id;
+
+			// Get the file info
+			let fileDoc;
+			const db = app.get('arango');
+			try {
+				fileDoc = await db.collection('files').document(fileId);
+			} catch (e) {
+				res.sendStatus(404);
+				return;
+			}
+
+			// Delete the file from B2
+			const b2 = app.get('b2');
+			await b2.authorize();
+			const b2FileId = fileDoc.b2FileId;
+			const b2FileName = 'files/' + fileId;
+			await b2.deleteFileVersion({
+				fileId: b2FileId,
+				fileName: b2FileName,
+			});
+
+			// Delete the file from Arango
+			await db.collection('files').remove(fileId);
+
+			res.sendStatus(204);
+
+		} catch (e) {
+			console.log(e);
+			res.sendStatus(500);
+		}
+	});
+	app.get('/files/:id/contents', async (req, res) => {
+		try {
+			// Get the file id
+			const fileId = req.params.id;
+
+			// Look up B2 fileId in Arango
+			const db = app.get('arango');
+			let b2FileId;
+			try {
+				const doc = await db.collection('files').document(fileId);
+				b2FileId = doc.b2FileId;
+			} catch (e) {
+				res.sendStatus(404);
+				return;
+			}
+
+			// Download file from B2
+			const b2 = app.get('b2');
+			await b2.authorize();
+			const b2Res = await b2.downloadFileById({ fileId: b2FileId });
+
+			res.send(b2Res.data);
 
 		} catch (err) {
 			console.log(err);
